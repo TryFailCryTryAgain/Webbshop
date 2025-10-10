a0ca07e49e286ffe0bd859e419bca2e1e3072758
TryFailCryTryAgain <david.k.segerbo@gmail.com>
2025-10-10 12:50:36 +0200

    updated models

---
+++ server/model/ProductModel.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Products extends Document {
+    _id: mongoose.Types.ObjectId;
+    title: string;
+    description: string;
+    price: number;
+    images: string[];
+    creation_at: Date;
+    update_at: Date;
+    rate: string[];
+}
+
+const ProductSchema: Schema = new Schema<Products>({
+    _id: { type: Schema.Types.ObjectId, auto:true },
+    title: { type: String },
+    description: { type: String },
+    price: { type: Number },
+    images: [{ type: String }],
+    creation_at: { type: Date },
+    update_at: { type: Date },
+    rate: [{ type: String }]
+})
---
+++ server/model/UserModel.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface Users extends Document {
+    _id: mongoose.Types.ObjectId;
+    email: string;
+    password: string;
+    adress: string;
+    ZIP: number;
+    city: string;
+    first_name: string;
+    last_name: string;
+    role: string;
+    tel: number;
+}
+
+const UserSchema: Schema = new Schema<Users>({
+    _id: { type: Schema.Types.ObjectId, auto:true },
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    tel: { type: Number },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    adress: { type: String },
+    ZIP: { type: Number },
+    city: { type: String }
+});
+
+export const User = mongoose.model<Users>('Users', UserSchema, 'User');
